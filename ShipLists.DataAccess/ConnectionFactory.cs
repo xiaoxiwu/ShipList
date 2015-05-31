b6e2f3d85c158bc1308e3176f5ae8341c71749a0
xiaoxiwu <xiaoxiwu2008@qq.com>
2015-05-31 15:32:57 +0800

    数据访问接口

---
+++ ShipLists.DataAccess/ConnectionFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipLists.DataAccess
+{
+   public class ConnectionFactory
+    {
+       private readonly static string connectionString = "";
+
+       private static ConnectionFactory ConnectionFactory = null;  
+    }
+}
